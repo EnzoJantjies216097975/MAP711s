0cbc889f7bd8d48daed83f9ace7d0820626eff06
EnzoJantjies216097975 <enzojantjies@gmail.com>
2025-05-22 12:49:37 +0200

    Profile Fixes

--- app/src/main/java/com/map711s/namibiahockey/screens/team/TeamScreen.kt
+++ app/src/main/java/com/map711s/namibiahockey/screens/team/TeamScreen.kt
@@ -1,7 +1,5 @@
 package com.map711s.namibiahockey.screens.team
 
-package com.map711s.namibiahockey.screens.team
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
